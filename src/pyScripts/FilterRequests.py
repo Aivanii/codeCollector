f7d5a346a79e29aabd75929c507e93702ae49d2f
gfhorigin <98154676+gfhorigin@users.noreply.github.com>
2024-10-14 20:03:05 +0700

    0.0.0.2

--- src/pyScripts/FilterRequests.py
+++ src/pyScripts/FilterRequests.py
@@ -8,6 +8,24 @@ class Filters:
     def remainder(self):
        # print(self.Path)
 
+        with open('../../filters.json', 'r', encoding='utf-8') as f:  # открыли файл с данными
+            self.filters = json.load(f)  # загнали все, что получилось в переменную
+
         print(os.listdir('../../public\Articles') )
-p = Filters(123)
+        self.PathFilter = [self.Path.split('/')[-3],self.Path.split('/')[-2] ]
+        print(self.PathFilter[0])
+        print(self.filters["Problems"]['DevOps']['Docker'] )
+        self.FindFilter()
+        print(self.FinalFilter)
+
+
+    def FindFilter(self):
+        if self.PathFilter[0] in self.filters["Language"]:
+            self.FinalFilter = self.filters["Language"][self.PathFilter[0]][self.PathFilter[1]]
+        elif self.PathFilter[0] in self.filters["Problems"]:
+            self.FinalFilter = self.filters["Problems"][self.PathFilter[0]][self.PathFilter[1]]
+
+
+
+p = Filters("http://localhost:5000/articles/DevOps/Docker/test_article")
 p.remainder()
