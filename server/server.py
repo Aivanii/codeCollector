75596ceade8d7bd8d2d14446a207d9a8a0722302
Aivanii <aivanellok@gmail.com>
2024-10-20 17:31:55 +0700

    added 1 article

--- server/server.py
+++ server/server.py
@@ -5,9 +5,9 @@ cors = CORS(app)
 @app.route('/articles/<filter1>/<filter2>/<article>', methods = ['GET'])
 def users(filter1,filter2,article):
     pul = __file__.split('\\')
+    print(article)
     with open(pul[0]+'/'+pul[1]+'/'+pul[2]+'/'+pul[3]+'/codeCollector/client/public/Articles/'+article+'.html', 'r', encoding='utf-8') as file:
         data = file.read()
-        print(data)
     return jsonify(
         {
             "users": [
